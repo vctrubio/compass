d5fddab618c508b152e319fa3f754f210613d3eb
vctrubio <vctrubio@gmail.com>
2025-05-16 17:17:03 +0200

    understand theme toggling. works

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/hero";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
@@ -6,9 +6,10 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <div className="flex justify-between items-center mb-2">
+          <h2 className="font-medium text-xl">Next steps</h2>
+        </div>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
     </>
